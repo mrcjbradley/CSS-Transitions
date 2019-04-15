e6e2b266759a17685fe636d6eefa66c9d728d283
mrcjbradley <cjbradley@me.com>
2019-04-15 17:18:02 -0400

    Refacter functions shared across effects

--- src/effects/MultiBoxMenu.js
+++ src/effects/MultiBoxMenu.js
@@ -2,26 +2,23 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { toggleLoading } from '../index';
 
 library.add(fas, far, fab); 
-
-
-
-// const box = findIconDefinition({ prefix: 'fas', iconName: 'box' });
-
-
 
 class MultiBoxMenu {
     constructor(content){
         this.content = content;
     }
 
-    jsRecognize(){
+    jsRecognize(toggleLoading){
         const menuLis = Array.from(document.getElementsByClassName('js-menu-li'));
         const menuUl = menuLis[0].parentElement;
         const toggleButton = document.getElementsByClassName('js-menu-toggle')[0];
         let openMenu = false;
-        toggleButton.addEventListener("click", () => {
+        
+        
+        const toggleMultiBoxMenu = () => {
             for (let idx = 0; idx < menuLis.length; idx++) {
                 const element = menuLis[idx];
                 if (openMenu){
@@ -46,13 +43,31 @@ class MultiBoxMenu {
                         case 3: element.style.height = '40%'; break;
                         case 4: element.style.width = '50%'; break;
                     }
-                    element.innerHTML = menuOptions[idx];
                     document.getElementById("menuIcon").innerHTML = `<i class="fas fa-box-open"> </i>`;
                 }
             }
+
+            menuLis.forEach((li, idx) => li.innerHTML = menuOptions[idx]);
+            
+            Array.from(document.getElementsByClassName('effect-btn')).forEach(btn => btn.addEventListener("click", (e) => {
+                e.preventDefault();
+                toggleLoading();
+                setTimeout(() => {
+                    toggleMultiBoxMenu();
+                    document.querySelector('body').classList = btn.getAttribute("data-body-class");
+                }, 1000);
+                setTimeout(() => {
+                    toggleLoading();
+                }, 2000);
+            }));
+
             openMenu = !openMenu;
             dom.i2svg();
-    }); 
+    };
+    
+    toggleButton.addEventListener("click", toggleMultiBoxMenu);
+
+
     dom.i2svg();
     this.activateBGMotion();
     }
@@ -67,40 +82,37 @@ class MultiBoxMenu {
         const moveBackground = () => {
             x += (lFollowX - x) * friction;
             y += (lFollowY - y) * friction;
-
             const translate = "translate(" + x + "px, " + y + "px) scale(1.1)";
             document.getElementsByClassName('bg')[0].style.transform = translate;
-            // console.log(translate);
         };
-
-        // debugger
-        // document.getElementsByClassName('bg')[0].addEventListener('mousemove', (e) => {
         window.addEventListener('mousemove', (e) => {
             const lMouseX = Math.max(-100, Math.min(100, window.innerWidth / 2 - e.clientX));
             const lMouseY = Math.max(-100, Math.min(100, window.innerHeight / 2 - e.clientY));
             lFollowX = (20 * lMouseX) / 150;
             lFollowY = (10 * lMouseY) / 150;
-
             moveBackground();
         });
     }
-
     render(hook){
         hook.innerHTML = this.content;
-
-        
     }
 }
 
 
 const menuOptions = [
     `   <ul>
-        <li><a href="#" class="effect-link txt-tf-upper ls-7 fs-1">Card Flip Grid Menu</a></li>
-        <li><a href="#" class="effect-link txt-tf-upper ls-7 fs-1">Slide Show Reveal</a></li>
-        <li><a href="#" class="effect-link txt-tf-upper ls-5 fs-1">HoverGrid Expand</a></li>
-        <li><a href="#" class="effect-link txt-tf-upper ls-4 fs-1">Multi Box Menu</a></li>
-        <li><a href="#" class="effect-link txt-tf-upper ls-3 fs-1">Hover Image</a></li>
-        <li><a href="#" class="effect-link txt-tf-upper ls-2 fs-1">Glitch</a></li>
+        <li class="efftect-link-lable">
+            for menus
+        </li>
+        <li><a href="#" class="effect-btn effect-link txt-tf-upper" data-body-class="sliding-box-menu-effect">Sliding Box Menu</a></li>
+        <li><a href="#" class="effect-btn effect-link txt-tf-upper" data-body-class="collapsing-menu-effect">Collapsing Menu</a></li>
+        <li><a href="#" class="effect-btn effect-link txt-tf-upper" data-body-class="multi-box-menu-effect">Multi Box Menu</a></li>
+        <li class="efftect-link-lable">
+            for content
+        </li>
+        <li><a href="#" class="effect-btn effect-link txt-tf-upper" data-body-class="">Hover Reveal</a></li>
+        <li><a href="#" class="effect-btn effect-link txt-tf-upper" data-body-class="">Slide Show</a></li>
+        <li><a href="#" class="effect-btn effect-link txt-tf-upper" data-body-class="">Card Flip</a></li>
         </ul>
     `,
     `
@@ -113,10 +125,10 @@ const menuOptions = [
         <a href="" class="contact-link">Get in touch now →</a>
     `,
     `   <ul>
-        <li><a href="" class="effect-element-link fs-3-5 txt-tf-lower"><span class="ol-super-number">01</span> <span>Menu Animation</span></a></li>
-        <li><a href="" class="effect-element-link fs-3-5 txt-tf-lower"><span class="ol-super-number">02</span> <span>Background Animation</span></a></li>
-        <li><a href="" class="effect-element-link fs-3-5 txt-tf-lower"><span class="ol-super-number">03</span> <span>Content Styling</span></a></li>
-        <li><a href="" class="effect-element-link fs-3-5 txt-tf-lower"><span class="ol-super-number">04</span> <span>More...</span></a></li>
+        <li><a href="" class="effect-element-link txt-tf-lower"><span class="ol-super-number">01</span> <span>Menu Animation</span></a></li>
+        <li><a href="" class="effect-element-link txt-tf-lower"><span class="ol-super-number">02</span> <span>Background Animation</span></a></li>
+        <li><a href="" class="effect-element-link txt-tf-lower"><span class="ol-super-number">03</span> <span>Content Styling</span></a></li>
+        <li><a href="" class="effect-element-link txt-tf-lower"><span class="ol-super-number">04</span> <span>More...</span></a></li>
         </ul>
     `
 ];
--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import MultiBoxMenu from './effects/MultiBoxMenu';
 import './styles/app.scss';
 
 document.addEventListener("DOMContentLoaded", () => {
-    // console.log('hello');
-    document.querySelector('body').innerHTML = startingContent;
-    const effectBtns = Array.from(document.getElementsByClassName('effect-btn'));
+    document.querySelector('body').innerHTML = startingLoadWrapperContent;
     const loadingContainer = document.getElementsByClassName('loading-container')[0];
-    const loadingAnimation = document.getElementsByClassName('loading-animation')[0];
-    const navLinks = Array.from(document.getElementsByClassName('nav-link'));
-    const demoSuperNav = document.getElementsByClassName('demo-super-nav')[0];
-    const burger = document.getElementById("burger-box");
+    
     let openMenu = false;
-    
-    
     let loading = false;
-    
+
     const toggleLoading = () => {
         if (loading) {
             loadingContainer.style.transformOrigin = "bottom";
@@ -26,7 +19,20 @@ document.addEventListener("DOMContentLoaded", () => {
         loading = !loading;
     };
 
+    toggleLoading();
+    document.getElementById('starting-content-container').innerHTML = startingContent;
+       const demo = document.getElementById('demo');
 
+       MultiBoxMenu.render(demo);
+       MultiBoxMenu.jsRecognize(toggleLoading);
+    setTimeout(toggleLoading, 1000);
+        
+    // debugger
+
+    const effectBtns = Array.from(document.getElementsByClassName('effect-btn'));
+    const navLinks = Array.from(document.getElementsByClassName('nav-link'));
+    const demoSuperNav = document.getElementsByClassName('demo-super-nav')[0];
+    const burger = document.getElementById("burger-box");
 
     burger.addEventListener("click", (e) => {
         if (openMenu) {
@@ -43,57 +49,57 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         toggleLoading();
         setTimeout(() => {
-            toggleLoading();
             document.querySelector('body').classList = btn.getAttribute("data-body-class");
+        }, 500);
+        setTimeout(() => {
+            toggleLoading();
         }, 2000);
     }));
 
+    // ======= START:  handle initial active selection ======= //
+
     const startingSect = location.hash.length > 1 ? location.hash.slice(1) : 'home' ;
     const activeElem = document.getElementById('nav-link-' + startingSect);
-   
-        activeElem.classList.add('active');
-        demoSuperNav.classList.add( ['about', 'services'].includes(startingSect) ? 'lightblue' : 'white');
+    activeElem.classList.add('active');
+    demoSuperNav.classList.add( ['about', 'services'].includes(startingSect) ? 'lightblue' : 'white');
 
+    // ======= END:  handle initial active selection ======= //
+
+    // ======= START:  addEventListeners to Demo Nav Links ======= //
 
     navLinks.forEach((navLink, idx) => navLink.addEventListener('click', (e) => {
         navLinks.forEach((link, idx2) => {
             link.classList.remove('active');
         });
         navLink.classList.add('active');
-        // if(window.innerWidth > 800){
             demoSuperNav.classList.add(idx % 2 !== 0 ? "lightblue" : "white");
             demoSuperNav.classList.remove(idx % 2 === 0 ? "lightblue" : "white");
-        // }
-        // setTimeout( () => {
             demoSuperNav.classList.remove('open');
             burger.innerText = "menu";
             openMenu = !openMenu;
-        // }, 0);
     }));
 
-    // if (document.querySelector('body').classList[0] === 'multi-box-menu-effect'){
+    // ======= END:  addEventListeners to Demo Nav Links ======= //
 
-    const demo = document.getElementById('demo');
-
-    MultiBoxMenu.render(demo);
-    MultiBoxMenu.jsRecognize();
-
-    const menuLis = Array.from(document.getElementsByClassName('js-menu-li'));
-    const menuUl = menuLis[0].parentElement;
-
-// }
+ 
 
 });
 
 
-const startingContent = `
+const startingLoadWrapperContent = `
     <div id="demo"></div>
     <div class="loading-wrapper">
         <div class="loading-container">
             <div class="loading-animation" data-anim="loading" data-text="loading...">loading...</div>
         </div>
     </div>
-        <div class="effect-selection">
+    <div id="starting-content-container">
+        
+    </div>
+`;
+
+const startingContent = `
+    <div class="effect-selection">
             <span class="effect-btn" data-body-class="collapsing-menu-effect">Collapsing Menu</span>
             <span class="effect-btn" data-body-class="sliding-box-menu-effect">Sliding Box Menu</span>
             <span class="effect-btn" data-body-class="multi-box-menu-effect">Multi Box Menu</span>
@@ -101,7 +107,7 @@ const startingContent = `
             <!-- <span class="effect-btn" data-body-class="card-flip-effect">Card Flip</span> -->
             <!-- <span class="effect-btn" data-body-class="hover-reveal-effect">Hover Reveal</span> -->
             <!-- <span class="effect-btn" data-body-class="page-slide-effect">Page Slide</span> -->
-        </div>
+    </div>
 
 
 
