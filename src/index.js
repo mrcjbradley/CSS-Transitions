d3408df4a2f37f746efd297d4e8c77d8d5881eab
mrcjbradley <cjbradley@me.com>
2019-04-21 23:29:17 -0400

    Add profile links

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import MultiBoxMenu from './effects/MultiBoxMenu';
 import BuildLoadingAnimation from './BuildLoadingAnimation';
 import buildEffectSelection from './buildEffectSelection';
 
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+// import { fas } from '@fortawesome/free-solid-svg-icons';
+// import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab); //, far, fas); 
+
 document.addEventListener("DOMContentLoaded", () => {
     const loader = new BuildLoadingAnimation();
     const effectMenu = new buildEffectSelection('collapsing menu', 'sliding box menu', 'multi box menu');
-        
+    const profileLinksDiv = document.createElement('div');
+    profileLinksDiv.className = "profile-links";
+    effectMenu.effectSelection.append(profileLinksDiv);
+    profileLinksDiv.innerHTML = `
+        <a href="https://github.com/mrcjbradley/" target="_blank"><i class="fab fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/collin-james-bradley-a87763a0/" target="_blank"><i class="fab fa-linkedin"></i></a>
+    `;
+    dom.i2svg();
     const thatBod = document.querySelector('body');
     loader.render(thatBod);
         effectMenu.render(thatBod);
